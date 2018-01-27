1a8f331ab449ab30efe7fb2a0fcba60754bc8c52
Passion1999 <35870640+Passion1999@users.noreply.github.com>
2018-01-27 22:09:27 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,19 +11,19 @@ public class Main {
 	static  InVentory i3 =new ConsInventory(doll, i2);
 
 	public static void main(String [] args) {
-//		System.out.println(i1.raiseRate(0.05));
-//		System.out.println(i2.raiseRate(0.8));
-//		System.out.println(i3.raiseRate(0.09));
-//		
-//		System.out.println(i1.averagePrice());
-//		System.out.println(i2.averagePrice());
-//		System.out.println(i3.averagePrice());
-//		System.out.println(empty.averagePrice());
-//		
-//		System.out.println(empty.replaceName());
-//		System.out.println(i1.replaceName());
-//		System.out.println(i2.replaceName());
-//		System.out.println(i3.replaceName());
+		System.out.println(i1.raiseRate(0.05));
+		System.out.println(i2.raiseRate(0.8));
+		System.out.println(i3.raiseRate(0.09));
+		
+		System.out.println(i1.averagePrice());
+		System.out.println(i2.averagePrice());
+		System.out.println(i3.averagePrice());
+		System.out.println(empty.averagePrice());
+		
+		System.out.println(empty.replaceName());
+		System.out.println(i1.replaceName());
+		System.out.println(i2.replaceName());
+		System.out.println(i3.replaceName());
 		System.out.println(i3);
 		System.out.println(i3.eliminale("doll"));
 		System.out.println(i2.eliminale("gun"));
